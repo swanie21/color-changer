a30c9d1da9defef613a834e01f1b698c8088c685
swanie21 <kswanie21@gmail.com>
2016-07-18 17:01:53 -0600

    Add media query

--- scripts/script.js
+++ scripts/script.js
@@ -2,9 +2,21 @@ var $randomButton =$('#random-button');
 var $resetButton = $('#reset-button');
 var $backgroundColor = $('#background');
 
-// $randomButton.on('click', function() {
-//
-// }
+var colors = ['00','33','66','99','cc','ff'];
+var rand = function() {
+    return Math.floor(Math.random()*6);
+};
+
+var randomColor = function() {
+    var r = colors[rand()];
+    var g = colors[rand()];
+    var b = colors[rand()];
+    return "#"+r+g+b;
+};
+
+$randomButton.on('click', function() {
+  $backgroundColor.css('backgroundColor', randomColor());
+});
 
 $resetButton.on('click', function() {
   $backgroundColor.css('backgroundColor', 'white');
