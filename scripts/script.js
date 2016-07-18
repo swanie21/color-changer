f26e7f3ef4bc2d8b4b899210674d57a5a0626496
swanie21 <kswanie21@gmail.com>
2016-07-18 17:39:08 -0600

    Change color array to include all colors

--- scripts/script.js
+++ scripts/script.js
@@ -2,20 +2,24 @@ var $randomButton =$('#random-button');
 var $resetButton = $('#reset-button');
 var $backgroundColor = $('#background');
 
-var colors = ['00','33','66','99','cc','ff'];
-var rand = function() {
-    return Math.floor(Math.random()*6);
+var colors = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
+var generateNumber = function() {
+    return Math.floor(Math.random()*16);
 };
 
 var randomColor = function() {
-    var r = colors[rand()];
-    var g = colors[rand()];
-    var b = colors[rand()];
-    return "#"+r+g+b;
+    var r1 = colors[generateNumber()];
+    var g1 = colors[generateNumber()];
+    var b1 = colors[generateNumber()];
+    var r2 = colors[generateNumber()];
+    var g2 = colors[generateNumber()];
+    var b2 = colors[generateNumber()];
+    return "#"+r1+r2+g1+g2+b1+b2;
 };
 
 $randomButton.on('click', function() {
   $backgroundColor.css('backgroundColor', randomColor());
+  console.log(color)
 });
 
 $resetButton.on('click', function() {
